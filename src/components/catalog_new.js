7a310f05386a389e7fc9f8d7c7e3ee3829430d50
jerry <jejerry@gotitapp.co>
2018-04-15 23:18:45 +0700

    Add CatalogNew component

--- src/components/catalog_new.js
+++ src/components/catalog_new.js
@@ -1,12 +1,71 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {Field, reduxForm} from 'redux-form';
 
 class CatalogNew extends Component {
+    renderField(field) { // field is object contain all handlers of Field component
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    // JSX syntax to handle all events of input like: onChange, onEnter, ...
+                    {...field.input}
+                />
+                {field.meta.error}
+            </div>
+        )
+    }
+
+    onSubmit(value) {
+        console.log(value);
+    }
 
     render() {
+        const {handleSubmit} = this.props;
         return (
-            <div>New Catalog</div>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                {/* Field is an abstract component of ReduxForm. It cannot do anything without
+                    without ReduxForm. Field helps us handling events */}
+                <Field
+                    label="Name"
+                    name="name"
+                    component={this.renderField} /* Field need component to tell ReduxForm
+                    how to render itself on screen */
+                />
+
+                <Field
+                    label="Description"
+                    name="description"
+                    component={this.renderField}
+                />
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to='/' className="btn btn-danger">Cancel</Link>
+            </form>
         )
     }
 }
 
-export default CatalogNew;
+function validate(values) {
+    const errors = {};
+
+    // validate inputs from 'values'
+    if (!values.name) {
+        errors.name = "Enter a title";
+    }
+
+    if (!values.description) {
+        errors.description = "Enter a description";
+    }
+
+    // if errors is empty, the form is fine to submit
+    // if errors has any properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'CatalogNewForm'
+})(CatalogNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import CatalogsReducer from './reducer_catalog';
 import ItemsReducer from './reducer_item';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
     catalogs: CatalogsReducer,
-    items: ItemsReducer
+    items: ItemsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
